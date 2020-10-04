86a6dade9b4cca576ddc5762075dec01915ed3c1
Fajar Fadillah Agustian <fajaragustian979@gmail.com>
2020-10-04 19:20:13 +0700

    project finish

--- js/script.js
+++ js/script.js
@@ -19,6 +19,53 @@ new hoverEffect({
   intensity: 0.3,
   image1: "img/img1.jpg",
   image2: "img/img2.jpg",
-  displacementImage: "assets/img/diss.png",
+  displacementImage: "img/diss.png",
   imagesRatio: 450 / 300,
+});
+
+// ----- GASP -----
+
+// NAV, TITLE, IMG
+gsap.from(".nav, .nav__item", {
+  opacity: 0,
+  duration: 1,
+  delay: 1,
+  y: -30,
+  stagger: 0.2,
+});
+
+gsap.from(".home__title", {
+  opacity: 0,
+  duration: 1.2,
+  delay: 1.2,
+  y: -30,
+});
+
+gsap.from(".home__img", {
+  opacity: 0,
+  duration: 1.3,
+  delay: 1.3,
+  y: -50,
+});
+
+// SOCIAL, SUBTITLE, BUTTON
+gsap.from(".home__social", {
+  opacity: 0,
+  duration: 1.5,
+  delay: 1,
+  y: 50,
+});
+
+gsap.from(".home__subtitle", {
+  opacity: 0,
+  duration: 1.8,
+  delay: 1,
+  y: 50,
+});
+
+gsap.from(".home__button", {
+  opacity: 0,
+  duration: 2,
+  delay: 1,
+  y: 50,
 });
